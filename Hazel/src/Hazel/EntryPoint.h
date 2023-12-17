bf4f1101a780f4918fd1e948a6b48f118c665b8b
gmpoop <carlos.basskame@gmail.com>
2023-12-16 23:37:01 -0600

    SetUp Application basic

---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+ 
+extern Hazel::Application* Hazel::CreateApplication(); 
+
+int main() {
+
+	auto app = Hazel::CreateApplication(); 
+	app->Run();
+	delete app;
+
+
+}
+
+
+
+#endif 
